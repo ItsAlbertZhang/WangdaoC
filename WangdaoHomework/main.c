b4deef56d480290b6c010f59193bd60b08c40a20
ItsAlbertZhang <itsalbertzhang@gmail.com>
2021-12-30 17:05:21 +0800

    Initial Commit

---
+++ WangdaoHomework/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  WangdaoHomework
+//
+//  Created by 雨轩 on 2021/12/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
